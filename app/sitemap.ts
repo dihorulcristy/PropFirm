d479d2e206f731a55c9db6d1353b2bbe3a7c3c48
Herghiligiu Cristian <herghiligiu.cristian@yahoo.ro>
2025-12-02 19:14:15 +0200

    Update domain to propfirms-hub.com in sitemap and robots.txt

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://propfirmhub.com'
+    const baseUrl = 'https://propfirms-hub.com'
 
     return [
         {
